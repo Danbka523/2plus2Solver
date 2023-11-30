0850f5e71f250f2ca26a2ccbe29183fb9d631305
Daniil Bashinskiy <gordonanet12@gmail.com>
2023-11-30 11:41:27 +0300

    now we have solver

--- Form1.cs
+++ Form1.cs
@@ -2,18 +2,16 @@ namespace _2plus2Solver
 {
     public partial class Form1 : Form
     {
+        Solver solver;
         public Form1()
         {
             InitializeComponent();
+            solver = new Solver();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(Plus(2,2).ToString(), "ответ");
-        }
-
-        int Plus(int a, int b) { 
-            return a + b;
+            MessageBox.Show(solver.Plus(2,2).ToString(), "ответ");
         }
     }
 }
---
+++ Solver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2plus2Solver
+{
+    internal class Solver
+    {
+        public Solver() { }
+
+        public int Plus(int a, int b) => a + b;
+    }
+}
